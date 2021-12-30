8f62b62fa2672b39a0245044b2740f5e4f7a42c3
jhhkim <aekim7@naver.com>
2021-12-30 12:05:58 +0900

    곡 클릭>유튜브 새창

--- src/main/resources/static/js/melon.js
+++ src/main/resources/static/js/melon.js
@@ -12,14 +12,47 @@ $(document).ready(function()
             html += field.title + "</a><td>";
             html += "<td>"+field.artist + "<td>";
             html += "</tr>";
+
+            
+
         });
         $("#musiclist").append(html);
+
+        //여기넣으면 =undifined 검색한 결과가 나오고 16행에 붙이면 1위곡 검색결과만 나옴
+        // $("#musiclist").click(function musicpop(field)
+        //     {   
+        //         link += field.title;
+        //         window.open(link);
+        //     }); 방법2로 해결!
+        
+        
     });
 });
 
-// $(document).click(function()
-// {   
-//     var url = "https://www.youtube.com/results?search_query="
-//     url += field.title;
-//     window.open(url);
+//방법2 window.open 사용
+// $(document).ready(function()
+// {
+//     $.getJSON("/api/v1/melon", function(result)
+//     {
+//         var html = "";
+//         var link = "https://www.youtube.com/results?search_query=";
+//         $.each(result, function(i, field)
+//         {
+//             html += "<tr>";
+//             html += "<td>"+field.rank + "<td>";
+//             html +="<td><a href= '#' onclick=\"musicPop('"+ link +  field.title + "')\">"
+//             html += field.title + "</a><td>";
+//             html += "<td>"+field.artist + "<td>";
+//             html += "</tr>";
+
+//         });
+//         $("#musiclist").append(html);
+        
+//     });
 // });
+
+
+// function musicPop(url)
+// {
+//     window.open(url);
+// }
